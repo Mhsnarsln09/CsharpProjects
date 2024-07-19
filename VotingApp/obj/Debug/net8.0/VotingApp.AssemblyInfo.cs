cdb54fa79ffd54f83e187c8005fbe524a7014301
Mhsnarsln09 <mhsnarsln09@gmail.com>
2024-07-19 17:05:36 +0300

    gitignore added.

--- VotingApp/obj/Debug/net8.0/VotingApp.AssemblyInfo.cs
+++ VotingApp/obj/Debug/net8.0/VotingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc290442cc295daee8ba44a703ab7d3def50a5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afeed9929273f4f9930e67d2e8c774349bf9573a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
